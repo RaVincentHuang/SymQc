000c52db5dee1ac6bed80c42d4fdacc3a7201085
Vincent <18573104293@163.com>
2021-12-21 22:35:16 +0800

    update the now state

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ C = [gate_call(gate(1, lib_call("X")), [0]), gate_call(gate(1, lib_call("Y")), [
 # sympy.pprint(make_U(A, 2).mat * make_U(B, 2).mat)
 # sympy.pprint(make_U(C, 2).mat)
 sympy.pprint(Q.state)
-Q = do_mul(Q, gate_call(gate(2, lib_call("CZ")), [0, 1]))
+X = gate_call(gate(2, lib_call("CZ")), [0, 1])
+Q = do_mul(Q, X)
+X = gate_call(gate(2, lib_call("CZ")), [0, 1])
 sympy.pprint(Q.state)
+
+
 # Q.state = make_U(g, 6).mat * Q.state
 # print(Q.state)
 # print("============================================================")
@@ -27,3 +31,35 @@ sympy.pprint(Q.state)
 # sympy.pprint(Q.state)
 # sympy.pprint(make_U(g, 6).mat)
 
+# 不同gate的实现
+class sim:
+    def __init__(self):
+        self.Q =
+        self.G_n =
+        self.circuit_global = circuit()
+
+
+    def apply(self):
+        pass
+
+class gate:
+    def __init__(self):
+        self.num_qubits = None
+        self.mat = None
+        self.ctrl_num = None
+        self.name = ""
+    def get_matrix(self):
+        pass
+
+
+
+class circuit:
+    def __init__(self):
+        self.qubit_list = []
+        self.gate_list = []
+
+while True:
+    sim.do(X, [0])
+    sim.do(Y, [1])
+    sim.Y()
+    sim.X()

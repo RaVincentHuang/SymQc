44f766fa27f67672d1f7b1504a08048686be6853
黎梓浩 <1245949348@qq.com>
2022-02-14 11:22:31 +0800

    pip related 3

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 setuptools.setup(name='symqc',
-      version='0.1.0',
+      version='0.0.1',
       description='Quantum computing simulator based on symbolic operation',
       url='https://gitee.com/huang-zixiao/qsim',
-      author='lzhHN',
+      author='13974811840',
       author_email='1245949348@qq.com',
       license='MIT',
       packages=setuptools.find_packages(),
